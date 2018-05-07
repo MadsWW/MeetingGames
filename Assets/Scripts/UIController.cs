6dbac52a02d47db93b5efb2430d6b2af418608c8
MadsWW <33266830+MadsWW@users.noreply.github.com>
2018-05-07 11:28:19 +0200

    UIChanges

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    private GameObject[] UIPanels;
+
+    private void Start()
+    {
+        UIPanels = GetComponentsInChildren<GameObject>();
+    }
+    public void EnablePanel(Panel panel)
+    {
+        foreach(GameObject gObject in UIPanels)
+        {
+            gObject.SetActive(false);
+        }
+
+        go.SetActive(true);
+    }
+}
